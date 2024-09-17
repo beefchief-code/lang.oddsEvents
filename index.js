fe5da02e606fdca7f3f8986c16be55d0441ca473
beefchief <c.lang.1993@gmail.com>
2024-09-17 16:26:50 -0500

    psuedocode written

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // TODO: this file! :)
+
+//STATE
+    //vars for each box
+const bank = [];
+const odds = [];
+const evens = [];
+
+//to add input number to bank
+function addToBank(num){
+    bank.push(num);
+}
+
+//sort odds and evens
+function numberSorter(){
+    //grab first number from bank
+    const num = bank.shift();
+    //if num % 2 === 0; => append to evens
+    if(num % 2 ===0){
+        evens.push(num);
+    }   else {
+        odds.push(num);
+    }
+    //else append to odd
+}
+//end state
+
+//RENDER
+    //push bank to <#numberBank>
+    //push odds to <#odds>
+    //push evens to <#evens>
+
+//SCRIPT
+    //sort 1: send first num in bank to odds/evens
+    //sort all: loop sort 1 for length of bank
+
